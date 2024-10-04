63251e2d9af802f945b19bb2eac720f7326bd0ab
Your Name <you@example.com>
2024-10-04 21:33:36 +0530

    added all files

---
+++ task/script.js
@@ -0,0 +1,70 @@
+//1st
+var a="sunday";
+switch(a){
+    case "monday":
+        console.log("working day");
+    case "tuesday":
+        console.log("working day");
+    case "wednesday":
+        console.log("working day");
+    case "thursday":
+            console.log("working day");
+    case "friday":
+        console.log("working day");
+    case "saturday":
+        console.log("working day");
+    case "sunday":
+        console.log("Holiday");
+        break;
+   default:
+    console.log("not a day");
+    
+}
+//2nd
+var a="August";
+switch(a){
+    case "January":
+        console.log("Jan");
+    case "February":
+        console.log("feb");
+    case "March":
+        console.log("march");
+    case "April":
+            console.log("april");
+    case "May":
+        console.log("may");
+    case "June":
+        console.log("june");
+    case "July":
+        console.log("july");
+    case "August":
+            console.log("August");
+            break;
+    case "September":
+            console.log("Sep");
+    case "October":
+            console.log("oct");
+    case "November":
+                console.log("Nov");
+    case "December":
+            console.log("Dec");
+   default:
+    console.log("not a day");
+    
+}
+//3rd
+var num=1;
+while (num <= 10){
+    if(num%2==0){
+    console.log(num)
+    }
+    num++;
+}
+//4th
+let countdown = 10;
+
+while (countdown >= 0) {
+    console.log(countdown);
+    countdown--;
+}
+
